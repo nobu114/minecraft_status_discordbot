1bc67e9d3987e5a7576f1a151c3a25065a56d5b5
nobu <nao114computer@gmail.com>
2023-08-28 21:56:24 +0900

    Fix: コードの修正

--- main.py
+++ main.py
@@ -18,14 +18,26 @@ MC_SERVER_ADDRESS = os.environ.get("MC_SERVER_ADDRESS")
 if TOKEN is None:
     print(".envファイルにTOKENを設定して下さい")
     sys.exit(1)
-if MC_SERVER_ADDRESS is None:
-    print(".envファイルにMC_SERVER_ADDRESSを設定して下さい")
-    sys.exit(1)
-    
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
+        if MC_SERVER_ADDRESS is None:
+            print(".envファイルにMC_SERVER_ADDRESSを設定して下さい")
+            sys.exit(1)
         print(f"Logged on as {self.user}")
+        while True:
+            ping = PINGClient(MC_SERVER_ADDRESS)
+            try:
+                stats = ping.get_stats()
+                numplayers = stats["players"]["online"]
+                game = discord.Game(f"{numplayers}人がオンライン")
+                status = discord.Status.online
+            except:
+                game = discord.Game("サーバーはオフライン")
+                status = discord.Status.idle
+                await client.change_presence(status=status, activity=game)
+                await asyncio.sleep(1)
 
     async def on_message(self, message):
         print(f"Message from {message.author}: {message.content}")
@@ -36,17 +48,3 @@ intents.message_content = True
 
 client = MyClient(intents=intents)
 client.run(TOKEN)
-
-
-while True:
-    ping = PINGClient(MC_SERVER_ADDRESS)
-    try:
-        stats = ping.get_stats()
-        numplayers = stats["players"]["online"]
-        game = discord.Game(f"{numplayers}人がオンライン")
-        status = discord.Status.online
-    except:
-        game = discord.Game("サーバーはオフライン")
-        status = discord.Status.idle
-    asyncio.run(client.change_presence(status=status, activity=game))
-    time.sleep(1)
